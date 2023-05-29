fe5394f2b52a0f317c91a53f04213a8981c6e326
Eduardo6446 <eduorozco8@gmail.com>
2023-05-28 21:15:52 -0600

    final

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -1,7 +1,13 @@
+import 'package:animated_flip_counter/animated_flip_counter.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +20,43 @@ class HomeScreen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text("Counter", style: TextStyle(fontSize: 24)),
-          Text('0', style: TextStyle(fontSize: 22, color: Colors.black54)),
+          AnimatedFlipCounter(
+            duration: Duration(milliseconds: 500),
+            value: counter,
+            textStyle: TextStyle(fontSize: 50),
+          )
         ],
       )),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            print('sirve');
-          }),
+      floatingActionButton: _buttons(),
+    );
+  }
+
+  Widget _buttons() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              setState(() => counter++);
+            }),
+        SizedBox(height: 10),
+        FloatingActionButton(
+            child: Icon(Icons.remove),
+            backgroundColor: Colors.red,
+            onPressed: () {
+              if (counter > 0) {
+                setState(() => counter--);
+              }
+            }),
+        SizedBox(height: 10),
+        FloatingActionButton(
+            child: Icon(Icons.exposure_zero),
+            backgroundColor: Colors.green,
+            onPressed: () {
+              setState(() => counter = 0);
+            }),
+      ],
     );
   }
 }
